39cf7b7f8bc68716e020e0fb59fad2afe1de39a9
abhilash <abhilash@ordant.com>
2018-04-22 13:07:07 +0530

    Added RestService
    Added RestService a wrapper for making easier REST API calls

---
+++ src/Exceptions/SocketException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RestService\Exceptions;
+
+use ErrorException;
+
+/**
+ * Class SocketException
+ * @package RestService\Exceptions
+ */
+class SocketException extends ErrorException
+{
+}
